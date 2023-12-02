24cf52de0c584e99892d8f20ecaabe9ed366c019
attenuatoriit <124893107+attenuatoriit@users.noreply.github.com>
2023-12-03 04:10:12 +0530

    Update mpu_roll_pitch.ino

--- mpu_roll_pitch.ino
+++ mpu_roll_pitch.ino
@@ -1,29 +1,37 @@
 #include <Wire.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Int32MultiArray.h>
+ros::NodeHandle node_handle;
+std_msgs::Int32MultiArray button_msg;
+ros::Publisher button_publisher("mpu_data", &button_msg);
+
 float RateRoll, RatePitch, RateYaw;
 float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
 int RateCalibrationNumber;
-float previouserror=0,error=0,pwm=0;
+float previouserror = 0, error = 0, pwm = 0;
 float AccX, AccY, AccZ;
 float sum = 0;
 float kp = 220;
 float ki = 0.09;
-float kd=0.001; 
-int en1=5;
-int in1=2;
-int in2=7;
+float kd = 0.001;
+int en1 = 5;
+int in1 = 2;
+int in2 = 7;
 float AngleRoll, AnglePitch;
 uint32_t LoopTimer;
-float KalmanAngleRoll=0, KalmanUncertaintyAngleRoll=2*2;
-float KalmanAnglePitch=0, KalmanUncertaintyAnglePitch=2*2;
-float Kalman1DOutput[]={0,0};
+float KalmanAngleRoll = 0, KalmanUncertaintyAngleRoll = 2 * 2;
+float KalmanAnglePitch = 0, KalmanUncertaintyAnglePitch = 2 * 2;
+float Kalman1DOutput[] = { 0, 0 };
 void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
-  KalmanState=KalmanState+0.004*KalmanInput;
-  KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
-  float KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 3 * 3);
-  KalmanState=KalmanState+KalmanGain * (KalmanMeasurement-KalmanState);
-  KalmanUncertainty=(1-KalmanGain) * KalmanUncertainty;
-  Kalman1DOutput[0]=KalmanState; 
-  Kalman1DOutput[1]=KalmanUncertainty;
+  KalmanState = KalmanState + 0.004 * KalmanInput;
+  KalmanUncertainty = KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  float KalmanGain = KalmanUncertainty * 1 / (1 * KalmanUncertainty + 3 * 3);
+  KalmanState = KalmanState + KalmanGain * (KalmanMeasurement - KalmanState);
+  KalmanUncertainty = (1 - KalmanGain) * KalmanUncertainty;
+  Kalman1DOutput[0] = KalmanState;
+  Kalman1DOutput[1] = KalmanUncertainty;
 }
 void gyro_signals(void) {
   Wire.beginTransmission(0x68);
@@ -36,30 +44,30 @@ void gyro_signals(void) {
   Wire.endTransmission();
   Wire.beginTransmission(0x68);
   Wire.write(0x3B);
-  Wire.endTransmission(); 
-  Wire.requestFrom(0x68,6);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 6);
   int16_t AccXLSB = Wire.read() << 8 | Wire.read();
   int16_t AccYLSB = Wire.read() << 8 | Wire.read();
   int16_t AccZLSB = Wire.read() << 8 | Wire.read();
   Wire.beginTransmission(0x68);
-  Wire.write(0x1B); 
+  Wire.write(0x1B);
   Wire.write(0x8);
-  Wire.endTransmission();     
+  Wire.endTransmission();
   Wire.beginTransmission(0x68);
   Wire.write(0x43);
   Wire.endTransmission();
-  Wire.requestFrom(0x68,6);
-  int16_t GyroX=Wire.read()<<8 | Wire.read();
-  int16_t GyroY=Wire.read()<<8 | Wire.read();
-  int16_t GyroZ=Wire.read()<<8 | Wire.read();
-  RateRoll=(float)GyroX/65.5;
-  RatePitch=(float)GyroY/65.5;
-  RateYaw=(float)GyroZ/65.5;
-  AccX=(float)AccXLSB/4096;
-  AccY=(float)AccYLSB/4096;
-  AccZ=(float)AccZLSB/4096;
-  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
-  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+  Wire.requestFrom(0x68, 6);
+  int16_t GyroX = Wire.read() << 8 | Wire.read();
+  int16_t GyroY = Wire.read() << 8 | Wire.read();
+  int16_t GyroZ = Wire.read() << 8 | Wire.read();
+  RateRoll = (float)GyroX / 65.5;
+  RatePitch = (float)GyroY / 65.5;
+  RateYaw = (float)GyroZ / 65.5;
+  AccX = (float)AccXLSB / 4096;
+  AccY = (float)AccYLSB / 4096;
+  AccZ = (float)AccZLSB / 4096;
+  AngleRoll = atan(AccY / sqrt(AccX * AccX + AccZ * AccZ)) * 1 / (3.142 / 180);
+  AnglePitch = -atan(AccX / sqrt(AccY * AccY + AccZ * AccZ)) * 1 / (3.142 / 180);
 }
 void setup() {
   Serial.begin(57600);
@@ -68,55 +76,58 @@ void setup() {
   Wire.setClock(400000);
   Wire.begin();
   delay(250);
-  Wire.beginTransmission(0x68); 
+  Wire.beginTransmission(0x68);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
-  for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+  for (RateCalibrationNumber = 0; RateCalibrationNumber < 2000; RateCalibrationNumber++) {
     gyro_signals();
-    RateCalibrationRoll+=RateRoll;
-    RateCalibrationPitch+=RatePitch;
-    RateCalibrationYaw+=RateYaw;
+    RateCalibrationRoll += RateRoll;
+    RateCalibrationPitch += RatePitch;
+    RateCalibrationYaw += RateYaw;
     delay(1);
   }
-  RateCalibrationRoll/=2000;
-  RateCalibrationPitch/=2000;
-  RateCalibrationYaw/=2000;
-  LoopTimer=micros();
+  RateCalibrationRoll /= 2000;
+  RateCalibrationPitch /= 2000;
+  RateCalibrationYaw /= 2000;
+  LoopTimer = micros();
+  node_handle.initNode();
+  node_handle.advertise(button_publisher);
 }
 void loop() {
   gyro_signals();
-  RateRoll-=RateCalibrationRoll;
-  RatePitch-=RateCalibrationPitch;
-  RateYaw-=RateCalibrationYaw;
+  RateRoll -= RateCalibrationRoll;
+  RatePitch -= RateCalibrationPitch;
+  RateYaw -= RateCalibrationYaw;
   kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
-  KalmanAngleRoll=Kalman1DOutput[0]; 
-  KalmanUncertaintyAngleRoll=Kalman1DOutput[1];
+  KalmanAngleRoll = Kalman1DOutput[0];
+  KalmanUncertaintyAngleRoll = Kalman1DOutput[1];
   kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
-  KalmanAnglePitch=Kalman1DOutput[0]; //calculates the pitch angle
-  KalmanUncertaintyAnglePitch=Kalman1DOutput[1];
+  KalmanAnglePitch = Kalman1DOutput[0];  //calculates the pitch angle
+  KalmanUncertaintyAnglePitch = Kalman1DOutput[1];
   Serial.print("Roll Angle [°] ");
   Serial.print(KalmanAngleRoll);
   Serial.print(" Pitch Angle [°] ");
   Serial.println(KalmanAnglePitch);
-  error=-KalmanAnglePitch;
+  button_msg.data[0] = KalmanAngleRoll;
+  button_msg.data[1] = KalmanAnglePitch;
+  button_publisher.publish( &button_msg );
+  node_handle.spinOnce();
+  error = -KalmanAnglePitch;
   sum = sum + error;
-  pwm=error*kp + sum*ki +(previouserror-error)*kd;
-   if(-KalmanAnglePitch<=0)
-       {       
-        analogWrite(en1,-pwm);
-      
-        digitalWrite(in1,LOW);
-        digitalWrite(in2,HIGH);
-       }
-      if(-KalmanAnglePitch>0)
-       {       
-        analogWrite(en1,pwm);
-//        digitalWrite(in1,LOW);
-//        digitalWrite(in2,HIGH);
-        digitalWrite(in1,HIGH);
-        digitalWrite(in2,LOW);
-       }
-     previouserror=error;
- 
+  pwm = error * kp + sum * ki + (previouserror - error) * kd;
+  if (-KalmanAnglePitch <= 0) {
+    analogWrite(en1, -pwm);
+
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+  if (-KalmanAnglePitch > 0) {
+    analogWrite(en1, pwm);
+    //        digitalWrite(in1,LOW);
+    //        digitalWrite(in2,HIGH);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  previouserror = error;
 }
